364d139ca5b7c049dcc33e7a36acfeadaf60e775
cesar <cesarpazol1029@gmail.com>
2023-10-25 23:34:28 -0500

    Gateway tested, change api books methods for simplify

--- Gateaway/src/main/java/com/cesar/Gateaway/GateawayApplication.java
+++ Gateaway/src/main/java/com/cesar/Gateaway/GateawayApplication.java
@@ -2,10 +2,28 @@ package com.cesar.Gateaway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GateawayApplication {
 
+	
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		
+		return builder.routes()
+				
+				.route( r -> r
+						.path("/books/**")
+						.uri( "http://localhost:8001" ))
+				.build();
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GateawayApplication.class, args);
 	}
