84e6c390e9090f9656f1d9d8eb29f8c0d142a552
cesar <cesarpazol1029@gmail.com>
2023-11-03 22:15:31 -0600

    Msvc genres

---
+++ Genres/src/main/java/com/cesar/Genres/GenresApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.Genres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenresApplication.class, args);
+	}
+
+}
---
+++ Genres/src/main/java/com/cesar/Genres/dto/Genre_DTO.java
@@ -0,0 +1,18 @@
+package com.cesar.Genres.dto;
+
+public class Genre_DTO {
+
+	private String name;
+
+	public Genre_DTO(String name) {
+		
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
