26ae681cee9df0f9f7cdd06f85de7c2b1da5c998
cesar <cesarpazol1029@gmail.com>
2023-11-09 18:59:25 -0600

    CircuitBreaker to OpenFeign in Editorials msvc

--- Genres/src/main/java/com/cesar/Editorials/feign/FeignBooks.java
+++ Genres/src/main/java/com/cesar/Editorials/feign/FeignBooks.java
@@ -1,14 +1,29 @@
 package com.cesar.Editorials.feign;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-books", path = "/books")
+import com.cesar.Editorials.feign.FeignBooks.Fallback;
+
+@FeignClient(name = "msvc-books", path = "/books", fallback = Fallback.class)
 public interface FeignBooks {
 
 	@GetMapping("/{editorialId}")
 	public List<String> getBooksByEditorialId(@PathVariable Long editorialId);
+	
+	
+	@Component
+	static class Fallback implements FeignBooks{
+
+		@Override
+		public List<String> getBooksByEditorialId(Long editorialId) {
+			
+			return Arrays.asList("No books...");
+		}
+	}
 }
